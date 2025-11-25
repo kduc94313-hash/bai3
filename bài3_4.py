0607255dab90134b6e5108fc0af2956b8777a112
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:35:48 +0700

    bai3_4

---
+++ bài3_4.py
@@ -0,0 +1,12 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+a = "Hello Guy!"
+
+def say(a):
+  a = "Vinh University"
+  print(a)
+
+say(a)
+print(a)
