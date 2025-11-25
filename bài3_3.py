faa6580be52a4fda77fb6ed38e28432d28862c00
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:34:38 +0700

    bai3_3

---
+++ bài3_3.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+def say_hello():
+  a = "Hello"
+  print(a)
+
+say_hello()
