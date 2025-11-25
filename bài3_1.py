6899fb6394f02aac2bcbb46c25f10978902dab8f
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:21:57 +0700

    bai3_1

---
+++ bài3_1.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+def sum(a, b):
+  print("sum = " + str(a + b))
+
+sum(4, 5)
+sum(3, 7)
