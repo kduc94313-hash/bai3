056b38d5f517699d1ab00d87d8cbd7ff6a82cc08
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:43:22 +0700

    Add student info and even/odd checker functions

---
+++ bài3_7.py
@@ -0,0 +1,11 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+def checkValue(n):
+  if n % 2 == 0:
+    print("Day la mot so chan")
+  else:
+    print("Day la mot so le")
+
+checkValue(6)
